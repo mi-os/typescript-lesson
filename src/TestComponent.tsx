c67387cf786241ef1d32448de86b9cd64a29d38d
mi-os <jbhjmy@yahoo.co.jp>
2020-09-28 22:52:38 +0900

    initial commit

---
+++ src/TestComponent.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+interface Props {
+  text: string;
+}
+
+interface UserData {
+  id: number;
+  name: string;
+}
+
+const TestComponent: React.FC<Props> = (props) => {
+  const [count, useCount] = useState<number | null>(null);
+  const [user, useUser] = useState<UserData>({ id: 1, name: "dummy" });
+  const [inputData, setInputData] = useState("");
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setInputData(e.target.value);
+  return (
+    <div>
+      <h1>{props.text}</h1>
+      <h1>{count}</h1>
+      <input type="text" value={inputData} onChange={handleInputChange} />
+      <h1>{inputData}</h1>
+    </div>
+  );
+};
+
+export default TestComponent;
